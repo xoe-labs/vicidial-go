18df012187c78a59231b8a97eebc744d90c402b1
David Arnold <dar@xoe.solutions>
2020-11-02 15:27:55 -0500

    WIP

---
+++ internal/app/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"context"
+	stdSQL "database/sql"
+	"log"
+	"fmt"
+	"time"
+
+	"github.com/inconshreveable/log15"
+
+	driver "github.com/go-sql-driver/mysql"
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-sql/pkg/sql"
+	"github.com/ThreeDotsLabs/watermill/message"
+	"github.com/ThreeDotsLabs/watermill/message/router/middleware"
+	"github.com/ThreeDotsLabs/watermill/message/router/plugin"
+	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
+	"github.com/CyCoreSystems/ari/v5"
+	"github.com/CyCoreSystems/ari/v5/client/native"
+)
+
+var (
+	// For this example, we're using just a simple logger implementation,
+	// You probably want to ship your own implementation of `watermill.LoggerAdapter`.
+	logger = watermill.NewStdLogger(false, false)
+	log = log15.New()
+)
+
+func main() {
+	db := createDB()
+
+	router, err := message.NewRouter(message.RouterConfig{}, logger)
+	if err != nil {
+		panic(err)
+	}
+
+	// SignalsHandler will gracefully shutdown Router when SIGTERM is received.
+	// You can also close the router by just calling `r.Close()`.
+	router.AddPlugin(plugin.SignalsHandler)
+
+	// Router level middleware are executed for every message sent to the router
+	router.AddMiddleware(
+		// CorrelationID will copy the correlation id from the incoming message's metadata to the produced messages
+		middleware.CorrelationID,
+
+		// The handler function is retried if it returns an error.
+		// After MaxRetries, the message is Nacked and it's up to the PubSub to resend it.
+		middleware.Retry{
+			MaxRetries:      3,
+			InitialInterval: time.Millisecond * 100,
+			Logger:          logger,
+		}.Middleware,
+
+		// Recoverer handles panics from handlers.
+		// In this case, it passes them as errors to the Retry middleware.
+		middleware.Recoverer,
+	)
+
+    // For simplicity, we are using the gochannel Pub/Sub here,
+    // You can replace it with any Pub/Sub implementation, it will work the same.
+    pubSub := gochannel.NewGoChannel(gochannel.Config{}, logger)
+	// Producing some incoming messages in background
+	go publishMessages(pubSub)
+
+
+	// Subscriber to ARI events
+	// ...
+	// Publisher to ARI events
+	// ...
+
+
+	// Publisher to manager db (status: 'QUEUE', 'DEAD')
+	// DEAD:
+    publisher, err := sql.NewPublisher(
+        db,
+        sql.PublisherConfig{
+            SchemaAdapter: sql.DefaultMySQLSchema{},
+        },
+        logger,
+    )
+    if err != nil {
+        panic(err)
+    }
+
+	// Subscriber to manager db
+	// QUEUE:
+	// DEAD:
+	subscriber, err := sql.NewSubscriber(
+		db,
+		sql.SubscriberConfig{
+			SchemaAdapter:    sql.DefaultMySQLSchema{},
+			OffsetsAdapter:   sql.DefaultMySQLOffsetsAdapter{},
+			InitializeSchema: false,
+		},
+		logger,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+
+	// AddHandler returns a handler which can be used to add handler level middleware
+	handler := router.AddHandler(
+		"struct_handler",          // handler name, must be unique
+		"incoming_messages_topic", // topic from which we will read events
+		subscriber,
+		"outgoing_messages_topic", // topic to which we will publish events
+		publisher,
+		structHandler{}.Handler,
+	)
+
+	// Handler level middleware is only executed for a specific handler
+	// Such middleware can be added the same way the router level ones
+	handler.AddMiddleware(func(h message.HandlerFunc) message.HandlerFunc {
+		return func(message *message.Message) ([]*message.Message, error) {
+			log.Println("executing handler specific middleware for ", message.UUID)
+
+			return h(message)
+		}
+	})
+
+	// just for debug, we are printing all messages received on `incoming_messages_topic`
+	router.AddNoPublisherHandler(
+		"print_incoming_messages",
+		"incoming_messages_topic",
+		pubSub,
+		printMessages,
+	)
+
+	// just for debug, we are printing all events sent to `outgoing_messages_topic`
+	router.AddNoPublisherHandler(
+		"print_outgoing_messages",
+		"outgoing_messages_topic",
+		pubSub,
+		printMessages,
+	)
+
+	// Now that all handlers are registered, we're running the Router.
+	// Run is blocking while the router is running.
+	ctx := context.Background()
+	if err := router.Run(ctx); err != nil {
+		panic(err)
+	}
+}
+
+func publishMessages(publisher message.Publisher) {
+	for {
+		msg := message.NewMessage(watermill.NewUUID(), []byte("Hello, world!"))
+		middleware.SetCorrelationID(watermill.NewUUID(), msg)
+
+		log.Printf("sending message %s, correlation id: %s\n", msg.UUID, middleware.MessageCorrelationID(msg))
+
+		if err := publisher.Publish("incoming_messages_topic", msg); err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second)
+	}
+}
+
+func printMessages(msg *message.Message) error {
+	fmt.Printf(
+		"\n> Received message: %s\n> %s\n> metadata: %v\n\n",
+		msg.UUID, string(msg.Payload), msg.Metadata,
+	)
+	return nil
+}
+
+type structHandler struct {
+	// we can add some dependencies here
+}
+
+func (s structHandler) Handler(msg *message.Message) ([]*message.Message, error) {
+	log.Println("structHandler received message", msg.UUID)
+
+	msg = message.NewMessage(watermill.NewUUID(), []byte("message produced by structHandler"))
+	return message.Messages{msg}, nil
+}
+
+
+func createDB() *stdSQL.DB {
+	conf := driver.NewConfig()
+	conf.Net = "tcp"
+	conf.User = "root"
+	conf.Addr = "mysql"
+	conf.DBName = "watermill"
+
+	db, err := stdSQL.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
+}
+
